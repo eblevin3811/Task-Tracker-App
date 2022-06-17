ab7014719c03debdaefd4be1555247f3b765f325
eblevin <graceblevins@gmail.com>
2022-06-16 22:30:24 -0400

    Created tests for 100% statement coverage of all entity classes

---
+++ src/test/java/com/example/demo/FolderTaskPairTests.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FolderTaskPairTests {
+
+    @Test
+    public void testEmptyConstructor(){
+        FolderTaskPair pair = new FolderTaskPair();
+        assertThat(pair.getTaskId() == 0 && pair.getFolderId() == 0);
+    }
+
+    @Test
+    public void testConstructor(){
+        FolderTaskPair pair = new FolderTaskPair(12, 34);
+        assertThat(pair.getFolderId() == 12 && pair.getTaskId() == 34);
+    }
+
+    @Test
+    public void testSetGetFolderId(){
+        FolderTaskPair pair = new FolderTaskPair();
+        pair.setFolderId(123);
+        assertThat(pair.getFolderId() == 123);
+    }
+
+    @Test
+    public void testSetGetId(){
+        FolderTaskPair pair = new FolderTaskPair();
+        pair.setId(123);
+        assertThat(pair.getId() == 123);
+    }
+
+    @Test
+    public void testGetSetTaskId(){
+        FolderTaskPair pair = new FolderTaskPair();
+        pair.setTaskId(123);
+        assertThat(pair.getTaskId() == 123);
+    }
+}
---
+++ src/test/java/com/example/demo/UserTaskPairTests.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UserTaskPairTests {
+
+    @Test
+    public void testEmptyConstructor(){
+        UserTaskPair pair = new UserTaskPair();
+        assertThat(pair.getUserId() == 0 && pair.getTaskId() == 0);
+    }
+
+    @Test
+    public void testConstructor(){
+        UserTaskPair pair = new UserTaskPair(123, 456);
+        assertThat(pair.getUserId() == 123 && pair.getTaskId() == 456);
+    }
+
+    @Test
+    public void testSetGetUserId(){
+        UserTaskPair pair = new UserTaskPair();
+        pair.setUserId(123);
+        assertThat(pair.getUserId() == 123);
+    }
+
+    @Test
+    public void testSetGetId(){
+        UserTaskPair pair = new UserTaskPair();
+        pair.setId(2);
+        assertThat(pair.getId() == 2);
+    }
+
+    @Test
+    public void testSetGetTaskId(){
+        UserTaskPair pair = new UserTaskPair();
+        pair.setTaskId(345);
+        assertThat(pair.getTaskId() == 345);
+    }
+}
